8b483440aacd5eeb4485db0195ffb7e33f47b404
Garry Hill <garry@magnetised.info>
2012-04-26 11:37:56 +0100

    Rename logins recipe and add metadata

---
+++ cookbooks/spontaneous_logins/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Garry Hill"
+maintainer_email "garry@magnetised.net"
+license          "MIT"
+description      "A simple user login generator for use with Spontaneous CMS"
+#long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
